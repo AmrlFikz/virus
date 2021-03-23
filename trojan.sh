6bcbb1a4d0af0d55a1cc63410c9dae3006309ee7
amirul06 <80541110+amirul06@users.noreply.github.com>
2021-03-23 23:49:50 +0800

    Add files via upload

---
+++ trojan.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+
+#warna
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+Black='\033[0;30m'
+
+echo
+toilet -f future "   Error303" | lolcat
+echo -e $red"    +=================================+"$yellow
+user="Adekhack"
+pass="trojan"
+while [ "$username" != "$user" ]
+do
+  read -p "Username : " username
+done
+
+while [ "$password" != "$pass" ]
+do
+  read -p "Password : " password
+done
+sleep 2
+echo -e $green
+figlet -f future " access granted "
+sleep 2
+clear
+echo
+echo -e $purple"
+	     ╔══╗╔═╗╔═╗─╔╗╔══╗╔═╦╗
+	     ╚╗╔╝║╬║║║║─║║║╔╗║║║║║
+	     ─║║─║╗╣║║║╔╣║║╠╣║║║║║
+	     ─╚╝─╚╩╝╚═╝╚═╝╚╝╚╝╚╩═╝"
+sleep 2
+echo -e $blue"       +================================+"
+echo -e $red"        [$white * $red]$white Author   : Error303"
+echo -e $red"        [$white * $red]$white Script   : Trojan Attack"
+echo -e $blue"       +================================+"
+echo -e $red
+echo "Sila tulis dengan benar dan betul..!!"
+echo -e $yellow
+
+nombor=""
+read -p "Masukkan Nombor Target : " nombor :
+while [ "$nombor" ]
+do
+echo -e $green"Mencoba send \033[33;1mvirus ke nombor \033[0;31m$nombor"
+done
